ed389fb72c61c3ae7d1e7b77799c7856e60f3149
KrashMello <joelenrikesoteldo@gmail.com>
2024-06-24 20:25:09 -0400

    feat: layout modify

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "../globals.css";
+import Link from "next/link";
 
 const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -10,31 +11,48 @@ export const metadata: Metadata = {
   description: "accounting application",
 };
 
+const menu = [
+  {
+    title: "Dashboard",
+    to: "/dashboard",
+  },
+  {
+    title: "Expenses",
+    to: "/dashboard",
+  },
+  {
+    title: "Incomes",
+    to: "/dashboard",
+  },
+];
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-white font-sans antialiased",
-          fontSans.variable,
-        )}
-      >
-        <div className="flex justify-center items-center h-10 bg-violet-400 max-w-400 max-h-20 shadow-md">
-          <h2 className="text-white font-bold ">Bienvenidos</h2>
+    <body
+      className={cn(
+        "min-h-screen bg-white font-sans antialiased",
+        fontSans.variable,
+      )}
+    >
+      <div className="flex justify-between items-center px-8 h-10 bg-gray-400 max-w-400 max-h-20 drop-shadow-lg">
+        <h2 className="text-white font-bol">KM CONTABLE</h2>
+      </div>
+      <div className="flex min-h-[calc(100vh-40px)] w-full">
+        <div className="bg-gray-800 w-64 pt-8 px-8 flex flex-col items-start gap-2 ">
+          {menu.map((m, i) => {
+            return (
+              <Link key={i} href={m.to} className="text-white font-bold">
+                {m.title}
+              </Link>
+            );
+          })}
         </div>
-        <div className="flex h-full min-h-full w-full">
-          <div className="bg-gray-800 w-[20%] pt-8 pl-12">
-            <h3 className="text-white font-bold">Home</h3>
-            <h3 className="text-white font-bold">Questions</h3>
-            <h3 className="text-white font-bold">Tags</h3>
-          </div>
-          <main>{children}</main>
-        </div>
-      </body>
-    </html>
+        {children}
+      </div>
+    </body>
   );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,86 @@
+import Pipe from "@/components/chart/pipe";
+import InfoBox from "@/components/ui/info-box";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>hola</h2>
+    <main className="w-full px-12 py-8 flex flex-col gap-4 ">
+      <div className="w-full flex justify-between ">
+        <InfoBox title="Income" color="green" amount={5000} currency="$" />
+        <InfoBox title="Expenses" color="red" amount={5000} currency="$" />
+        <InfoBox title="Other" color="yellow" amount={5000} currency="$" />
+      </div>
+      <div className="flex justify-between w-full">
+        <div className="overflow-hidden rounded-lg w-[45%]">
+          <div className="w-full h-10 bg-gray-400 text-white px-3 items-center flex">
+            <h3 className="text-lg font-semibold">Expenses</h3>
+          </div>
+          <Pipe></Pipe>
+        </div>
+        <div className="w-[45%] h-64 overflow-hidden rounded-lg">
+          <div className="w-full h-10 bg-gray-400 text-white px-3 items-center flex">
+            <h3 className="text-lg font-semibold">Expenses</h3>
+          </div>
+          <Table>
+            <TableCaption>A list of your recent invoices.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Invoice</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Method</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+      <div className="flex justify-between w-full">
+        <div className="overflow-hidden rounded-lg w-[45%]">
+          <div className="w-full h-10 bg-gray-400 text-white px-3 items-center flex">
+            <h3 className="text-lg font-semibold">Incomes</h3>
+          </div>
+          <Pipe></Pipe>
+        </div>
+        <div className="w-[45%] h-64 overflow-hidden rounded-lg">
+          <div className="w-full h-10 bg-gray-400 text-white px-3 items-center flex">
+            <h3 className="text-lg font-semibold">Incomes</h3>
+          </div>
+          <Table>
+            <TableCaption>A list of your recent invoices.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Invoice</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Method</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+      </div>
     </main>
   );
 }
