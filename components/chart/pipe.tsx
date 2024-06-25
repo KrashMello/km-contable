c1869c9ebb692b6bd22fea213db63332f1d10f6b
KrashMello <joelenrikesoteldo@gmail.com>
2024-06-24 20:24:51 -0400

    feat: chart donut component

---
+++ components/chart/pipe.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Doughnut } from "react-chartjs-2";
+import { ArcElement } from "chart.js";
+import { Chart as ChartJS, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export default function Pipe() {
+  const dataa = {
+    labels: ["otros", "comida", "ropa"],
+    datasets: [
+      {
+        label: "Expenses",
+        data: [300, 50, 100],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
+  return <Doughnut data={dataa} />;
+}
