593403a40f0d611c2de342084b4f3dc59f35cfe3
KrashMello <joelenrikesoteldo@gmail.com>
2024-06-26 06:53:39 -0400

    feat: border color in pipe chart

--- components/chart/pipe.tsx
+++ components/chart/pipe.tsx
@@ -18,6 +18,7 @@ export default function Pipe() {
           "rgb(54, 162, 235)",
           "rgb(255, 205, 86)",
         ],
+        borderColor: "black",
         hoverOffset: 4,
       },
     ],
