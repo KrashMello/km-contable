c0270c59820ae396b3180f7c20cbe457b0505267
KrashMello <joelenrikesoteldo@gmail.com>
2024-09-05 06:07:46 -0400

    feat: modals groups transation ready

--- components/ui/ModalsGroups.tsx
+++ components/ui/ModalsGroups.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { format } from "date-fns";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogTrigger,
   DialogHeader,
+  DialogTitle,
 } from "@/components/ui/dialog";
 import {
   Form,
@@ -27,14 +29,34 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { CalendarIcon } from "lucide-react";
+import { Calendar } from "./calendar";
+import { getCookie } from "cookies-next";
+import { useState } from "react";
 
-const FormSchema = z.object({
-  dateEntry: z.string().date(),
-  description: z.string(),
-  amount: z.number(),
-  typeId: z.number().int(),
-  accountId: z.number().int(),
-});
+const FormSchema = z
+  .object({
+    dateEntry: z.date(),
+    description: z.string(),
+    amount: z.string().min(1, { message: "El monto es requerido" }),
+    typeId: z
+      .string()
+      .min(1, { message: "El tipo de Transaccion es requerida" }),
+    accountId: z.string().min(1, { message: "La cuenta es requerida" }),
+  })
+  .required({
+    dateEntry: true,
+    amount: true,
+    typeId: true,
+    accountId: true,
+  });
+
 export default function ModalsGroups(props: {
   accounts: any;
   typeTransations: any;
@@ -42,26 +64,49 @@ export default function ModalsGroups(props: {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      dateEntry: "",
-      amount: 0,
+      dateEntry: new Date(),
+      amount: "",
       description: "",
-      typeId: 0,
-      accountId: 0,
+      typeId: "",
+      accountId: "",
     },
   });
+  const [open, setOpen] = useState(false);
+  const [errors, setErrors] = useState({});
   async function onSubmit(data: z.infer<typeof FormSchema>) {
-    console.log(data);
+    data.dateEntry = format(data.dateEntry, "yyyy-MM-dd");
+    let result = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/incomesAndExpenses/`,
+      {
+        method: "POST",
+        headers: {
+          "x-access-id": String(getCookie("auth")),
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      },
+    );
+    const status = await result.status;
+    result = await result.json();
+    if (status !== 200) {
+      setErrors(result.message);
+      return;
+    } else {
+      setOpen(false);
+    }
   }
   return (
     <div className="fixed left-[-25px] bottom-[32px] flex flex-col z-50 items-end justify-end w-screen  gap-8 h-screen">
-      <Dialog>
+      <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
-          <Button className="text-slate-200 bg-slate-700 focus-visible:ring-0 focus-visible:outline-none border rounded-full size-14 border-muted">
+          <Button className="focus-visible:ring-0 focus-visible:outline-none border rounded-full size-14 border-muted">
             +
           </Button>
         </DialogTrigger>
-        <DialogContent className="bg-slate-700 text-slate-200 rounded-lg">
-          <DialogHeader>Transaccion</DialogHeader>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Transaccion</DialogTitle>
+          </DialogHeader>
           <div>
             <Form {...form}>
               <form
@@ -72,16 +117,56 @@ export default function ModalsGroups(props: {
                   control={form.control}
                   name="dateEntry"
                   render={({ field }) => (
+                    <FormItem className="w-full flex flex-col">
+                      <FormLabel>Date of birth</FormLabel>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant={"outline"}
+                              className={cn(
+                                "w-full pl-3 text-left font-normal",
+                                !field.value && "text-muted-foreground",
+                              )}
+                            >
+                              {field.value ? (
+                                format(field.value, "PPP")
+                              ) : (
+                                <span>Pick a date</span>
+                              )}
+                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-full p-0" align="start">
+                          <Calendar
+                            mode="single"
+                            selected={field.value}
+                            onSelect={field.onChange}
+                            disabled={(date) =>
+                              date > new Date() || date < new Date("1900-01-01")
+                            }
+                          />
+                        </PopoverContent>
+                      </Popover>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="amount"
+                  render={({ field }) => (
                     <FormItem className="w-full">
-                      <FormLabel>Fecha</FormLabel>
+                      <FormLabel>Importe</FormLabel>
                       <FormControl>
-                        <Input
-                          type="date"
-                          className="focus-visible:outline-none focus-visible:ring-0 text-slate-700"
-                          placeholder="Enter your username."
-                          {...field}
-                        />
+                        <Input placeholder="Ingrese el importe" {...field} />
                       </FormControl>
+                      {errors.amount ? (
+                        <FormMessage>{errors.amount}</FormMessage>
+                      ) : (
+                        <FormMessage />
+                      )}
                       <FormMessage />
                     </FormItem>
                   )}
@@ -93,23 +178,30 @@ export default function ModalsGroups(props: {
                     <FormItem className="w-full">
                       <FormLabel>Cuenta</FormLabel>
                       <FormControl>
-                        <Select>
-                          <SelectTrigger className="w-full">
-                            <SelectValue placeholder="seleccione un tipo de cuenta" />
-                          </SelectTrigger>
-                          <SelectContent className="bg-slate-800 text-white">
-                            {props.accounts.map((account: any) => {
-                              return (
-                                <SelectItem
-                                  key={window.crypto.randomUUID()}
-                                  value={`${account.id}`}
-                                >
-                                  {account.name}
-                                </SelectItem>
-                              );
-                            })}
-                          </SelectContent>
-                        </Select>
+                        {props.accounts.length > 0 ? (
+                          <Select
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          >
+                            <SelectTrigger className="w-full">
+                              <SelectValue placeholder="seleccione un tipo de cuenta" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              {props.accounts.map((account: any) => {
+                                return (
+                                  <SelectItem
+                                    key={window.crypto.randomUUID()}
+                                    value={`${account.id}`}
+                                  >
+                                    {account.name}
+                                  </SelectItem>
+                                );
+                              })}
+                            </SelectContent>
+                          </Select>
+                        ) : (
+                          <h4>Debe registrar una cuenta</h4>
+                        )}
                       </FormControl>
                       <FormMessage />
                     </FormItem>
@@ -122,11 +214,14 @@ export default function ModalsGroups(props: {
                     <FormItem className="w-full">
                       <FormLabel>Tipo de Transaccion</FormLabel>
                       <FormControl>
-                        <Select>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
                           <SelectTrigger className="w-full">
                             <SelectValue placeholder="seleccione el tipo de transaccion" />
                           </SelectTrigger>
-                          <SelectContent className="bg-slate-800 text-white">
+                          <SelectContent>
                             {props.typeTransations.map((type: any) => {
                               return (
                                 <SelectItem
@@ -151,13 +246,13 @@ export default function ModalsGroups(props: {
                     <FormItem className="w-full">
                       <FormLabel>Description</FormLabel>
                       <FormControl>
-                        <Textarea
-                          className="focus-visible:outline-none focus-visible:ring-0 text-slate-700"
-                          placeholder="Descripcion"
-                          {...field}
-                        />
+                        <Textarea placeholder="Descripcion" {...field} />
                       </FormControl>
-                      <FormMessage />
+                      {errors.description ? (
+                        <FormMessage>{errors.description}</FormMessage>
+                      ) : (
+                        <FormMessage />
+                      )}
                     </FormItem>
                   )}
                 />
