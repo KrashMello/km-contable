c8afcc7841c912ee4911dd18ff1ea5886184778d
KrashMello <joelenrikesoteldo@gmail.com>
2024-09-05 14:06:21 -0400

    fix: middleware error not found cookie

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const currentUser: any = request.cookies.get("auth");
-  console.log();
+  let currentUser: any = request.cookies.get("auth");
+  if (currentUser)
+    await fetch(`${process.env.NEXT_PUBLIC_API_URL}/account`, {
+      method: "GET",
+      headers: { "x-access-id": currentUser.value },
+    }).then((res) => {
+      if (res.status !== 200) {
+        currentUser = null;
+        request.cookies.delete("auth");
+      }
+    });
+
   if (!currentUser && request.nextUrl.pathname !== "/")
     return NextResponse.redirect(new URL("/", request.url));
 
@@ -10,17 +20,7 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
 
   if (request.nextUrl.pathname === "/") return;
-
-  let data;
-  await fetch(`${process.env.NEXT_PUBLIC_API_URL}/account`, {
-    method: "GET",
-    headers: { "x-access-id": currentUser.value },
-  }).then((res) => {
-    if (res.status !== 200) data = null;
-    else data = true;
-    res.json();
-  });
-  if (data) return;
+  return;
 }
 
 export const config = {
