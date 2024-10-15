df7de990b9265b0bc5e50b61787ea7491f1bed30
KrashMello <joelenrikesoteldo@gmail.com>
2024-10-14 20:35:20 -0400

    feat: accounts create

--- components/modals/account.tsx
+++ components/modals/account.tsx
@@ -29,21 +29,19 @@ import {
 } from "@/components/ui/select";
 import { useEffect, useState } from "react";
 import { accountStore } from "@/store";
+import { categoryStore } from "@/store/category";
 
 const FormSchema = z
   .object({
     name: z.string().min(1, { message: "El nombre es requerido" }),
-    accountType: z
-      .string()
-      .min(1, { message: "El tipo de cuenta es requerido" }),
-    currencyType: z
+    accountId: z.string(),
+    currencyId: z.string(),
+    transactionTypeId: z
       .string()
       .min(1, { message: "El tipo de moneda es requerido" }),
   })
   .required({
     name: true,
-    accountType: true,
-    currencyType: true,
   });
 
 export default function ModalAccount() {
@@ -51,30 +49,36 @@ export default function ModalAccount() {
     resolver: zodResolver(FormSchema),
     defaultValues: {
       name: "",
-      accountType: "",
-      currencyType: "",
+      accountId: "",
+      transactionTypeId: "",
+      currencyId: "",
     },
   });
-  const getCurrency = accountStore((state) => state.getCurrency);
-  const getAccountType = accountStore((state) => state.getAccountType);
-  const createAccount = accountStore((state) => state.createAccount);
-  const currencys = accountStore((state) => state.currency);
-  const accountType = accountStore((state) => state.accountType);
+  const getCurrencyTypes = categoryStore((state) => state.getCurrencyTypes);
+  const getAccountsTypes = categoryStore((state) => state.getAccountsTypes);
+  const getCategoriesTypes = categoryStore((state) => state.getCategoriesTypes);
+  const createCategory = categoryStore((state) => state.createCategory);
+  const currencys = categoryStore((state) => state.currencyTypes);
+  const accountType = categoryStore((state) => state.accountsTypes);
+  const categoriesTypes = categoryStore((state) => state.categoriesTypes);
+  const [selectedCategory, setSelectedCategory] = useState("");
   useEffect(() => {
-    getCurrency();
-    getAccountType();
-  }, [getCurrency, getAccountType]);
+    getCurrencyTypes();
+    getAccountsTypes();
+    getCategoriesTypes();
+  }, [getCurrencyTypes, getAccountsTypes, getCategoriesTypes]);
   const [open, setOpen] = useState(false);
   const [errors, setErrors]: [
     {
       name?: string;
-      accountType?: string;
-      currencyType?: string;
+      accountId?: string;
+      transactionTypeId?: string;
+      currencyId?: string;
     },
     any,
   ] = useState({});
   async function onSubmit(data: z.infer<typeof FormSchema>) {
-    let [status, result] = await createAccount(data);
+    let [status, result] = await createCategory(data);
     if (status !== 200) {
       setErrors(result.message);
       return;
@@ -87,11 +91,11 @@ export default function ModalAccount() {
     <div className="flex flex-col items-end justify-end px-2">
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
-          <Button>Agregar Cuenta</Button>
+          <Button>Agregar Categoria</Button>
         </DialogTrigger>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle>Cuenta</DialogTitle>
+            <DialogTitle>Categoria</DialogTitle>
           </DialogHeader>
           <div>
             <Form {...form}>
@@ -121,80 +125,124 @@ export default function ModalAccount() {
                 />
                 <FormField
                   control={form.control}
-                  name="accountType"
+                  name="transactionTypeId"
                   render={({ field }) => (
                     <FormItem className="w-full">
-                      <FormLabel>Tipo de Cuenta</FormLabel>
-                      <FormControl>
-                        {accountType.length > 0 ? (
-                          <Select
-                            onValueChange={field.onChange}
-                            defaultValue={field.value}
-                          >
-                            <SelectTrigger className="w-full">
-                              <SelectValue placeholder="seleccione un tipo de cuenta" />
-                            </SelectTrigger>
-                            <SelectContent>
-                              {accountType.map((type: any) => {
-                                return (
-                                  <SelectItem
-                                    key={window.crypto.randomUUID()}
-                                    value={`${type.id}`}
-                                  >
-                                    {type.name}
-                                  </SelectItem>
-                                );
-                              })}
-                            </SelectContent>
-                          </Select>
-                        ) : (
-                          <h4>Debe registrar una cuenta</h4>
-                        )}
-                      </FormControl>
-                      {errors.accountType ? (
-                        <FormMessage>{errors.accountType}</FormMessage>
-                      ) : (
-                        <FormMessage />
-                      )}
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="currencyType"
-                  render={({ field }) => (
-                    <FormItem className="w-full">
-                      <FormLabel>Tipo de Moneda</FormLabel>
+                      <FormLabel>Tipo de Categoria</FormLabel>
                       <FormControl>
                         <Select
-                          onValueChange={field.onChange}
+                          onValueChange={(value) => {
+                            if (value === "2") {
+                              form.setValue("currencyId", "");
+                              form.setValue("accountId", "");
+                            }
+                            setSelectedCategory(value);
+                            field.onChange(value);
+                          }}
                           defaultValue={field.value}
                         >
                           <SelectTrigger className="w-full">
-                            <SelectValue placeholder="seleccione el tipo de transaccion" />
+                            <SelectValue placeholder="seleccione un tipo de cuenta" />
                           </SelectTrigger>
                           <SelectContent>
-                            {currencys.map((currency: any) => {
+                            {categoriesTypes.map((categoryType: any) => {
                               return (
                                 <SelectItem
-                                  key={currency.id}
-                                  value={`${currency.id}`}
+                                  key={window.crypto.randomUUID()}
+                                  value={`${categoryType.id}`}
                                 >
-                                  {currency.name}
+                                  {categoryType.name}
                                 </SelectItem>
                               );
                             })}
                           </SelectContent>
                         </Select>
                       </FormControl>
-                      {errors.currencyType ? (
-                        <FormMessage>{errors.currencyType}</FormMessage>
+                      {errors.transactionTypeId ? (
+                        <FormMessage>{errors.transactionTypeId}</FormMessage>
                       ) : (
                         <FormMessage />
                       )}
                     </FormItem>
                   )}
                 />
+                {selectedCategory === "1" && (
+                  <>
+                    <FormField
+                      control={form.control}
+                      name="accountId"
+                      render={({ field }) => (
+                        <FormItem className="w-full">
+                          <FormLabel>Tipo de Cuenta</FormLabel>
+                          <FormControl>
+                            <Select
+                              onValueChange={field.onChange}
+                              defaultValue={field.value}
+                            >
+                              <SelectTrigger className="w-full">
+                                <SelectValue placeholder="seleccione un tipo de cuenta" />
+                              </SelectTrigger>
+                              <SelectContent>
+                                {accountType.map((type: any) => {
+                                  return (
+                                    <SelectItem
+                                      key={window.crypto.randomUUID()}
+                                      value={`${type.id}`}
+                                    >
+                                      {type.name}
+                                    </SelectItem>
+                                  );
+                                })}
+                              </SelectContent>
+                            </Select>
+                          </FormControl>
+                          {errors.accountId ? (
+                            <FormMessage>{errors.accountId}</FormMessage>
+                          ) : (
+                            <FormMessage />
+                          )}
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="currencyId"
+                      render={({ field }) => (
+                        <FormItem className="w-full">
+                          <FormLabel>Tipo de Moneda</FormLabel>
+                          <FormControl>
+                            <Select
+                              onValueChange={field.onChange}
+                              defaultValue={field.value}
+                            >
+                              <SelectTrigger className="w-full">
+                                <SelectValue placeholder="seleccione el tipo de transaccion" />
+                              </SelectTrigger>
+                              <SelectContent>
+                                {currencys.map((currency: any) => {
+                                  return (
+                                    <SelectItem
+                                      key={currency.id}
+                                      value={`${currency.id}`}
+                                    >
+                                      {currency.name}
+                                    </SelectItem>
+                                  );
+                                })}
+                              </SelectContent>
+                            </Select>
+                          </FormControl>
+                          {errors.currencyId ? (
+                            <FormMessage>{errors.currencyId}</FormMessage>
+                          ) : (
+                            <FormMessage />
+                          )}
+                        </FormItem>
+                      )}
+                    />
+                  </>
+                )}
+
                 <Button type="submit" className="w-full">
                   Agregar
                 </Button>
