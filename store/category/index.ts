3d98bfc36de20b3ab31637c61edeea9f791448b5
KrashMello <joelenrikesoteldo@gmail.com>
2024-10-14 20:35:40 -0400

    feat: store categories

--- store/category/index.ts
+++ store/category/index.ts
@@ -4,6 +4,42 @@ import { Action, State } from "./type";
 
 export const categoryStore = create<State & Action>((set, get) => ({
   categories: [],
+  categoriesDebits: [],
+  categoriesTypes: [],
+  accountsTypes: [],
+  currencyTypes: [],
+  accounts: [],
+  createCategory: async (data) => {
+    let result: any = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/category`,
+      {
+        method: "POST",
+        headers: {
+          "x-access-id": String(getCookie("auth")),
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      },
+    );
+    const status = await result.status;
+    result = await result.json();
+    get().getCategories();
+    get().getAccounts();
+    return [status, result];
+  },
+  getAccounts: async () => {
+    let result: any = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/category/?transactionType=1`,
+      {
+        method: "GET",
+        headers: {
+          "x-access-id": String(getCookie("auth")),
+        },
+      },
+    );
+    result = await result.json();
+    set({ accounts: result });
+  },
   getCategories: async () => {
     let result: any = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/category/?transactionType=2`,
@@ -16,5 +52,57 @@ export const categoryStore = create<State & Action>((set, get) => ({
     );
     result = await result.json();
     set({ categories: result });
+  },
+  getCategoriesDebits: async () => {
+    let result: any = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/category/?transactionType=1`,
+      {
+        method: "GET",
+        headers: {
+          "x-access-id": String(getCookie("auth")),
+        },
+      },
+    );
+    result = await result.json();
+    set({ categoriesDebits: result });
+  },
+  getCategoriesTypes: async () => {
+    let result: any = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/category/transactionType`,
+      {
+        method: "GET",
+        headers: {
+          "x-access-id": String(getCookie("auth")),
+        },
+      },
+    );
+    result = await result.json();
+    set({ categoriesTypes: result });
+  },
+  getAccountsTypes: async () => {
+    let result: any = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/category/account`,
+      {
+        method: "GET",
+        headers: {
+          "x-access-id": String(getCookie("auth")),
+        },
+      },
+    );
+    result = await result.json();
+    set({ accountsTypes: result });
+  },
+  getCurrencyTypes: async () => {
+    let result: any = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/category/currency`,
+      {
+        method: "GET",
+        headers: {
+          "x-access-id": String(getCookie("auth")),
+        },
+      },
+    );
+    result = await result.json();
+    set({ currencyTypes: result });
   },
 }));
--- store/category/type.d.ts
+++ store/category/type.d.ts
@@ -2,10 +2,44 @@ export type State = {
   categories: {
     id: string;
     name: string;
-    accountType: { name: string };
+    account: { name: string };
+  }[];
+  accounts: {
+    id: string;
+    name: string;
+    account: { name: string };
+  }[];
+  categoriesDebits: {
+    id: string;
+    name: string;
+    account: { name: string };
+  }[];
+  categoriesTypes: {
+    id: string;
+    name: string;
+  }[];
+  accountsTypes: {
+    id: string;
+    name: string;
+  }[];
+  currencyTypes: {
+    id: string;
+    name: string;
+    abbreviation: string;
   }[];
 };
 
 export type Action = {
   getCategories: () => void;
+  getCategoriesDebits: () => void;
+  getCategoriesTypes: () => void;
+  getAccountsTypes: () => void;
+  getCurrencyTypes: () => void;
+  getAccounts: () => void;
+  createCategory: (data: {
+    name: string;
+    accountId: string;
+    currencyId: string;
+    transaction_typeId: string;
+  }) => promise<[number, any]>;
 };
